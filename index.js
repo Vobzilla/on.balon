2403f9f5f58ef0d43316e74c2378eb61cfcaacc8
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-11-15 18:34:21 +0100

    add swiper

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 /** @format */
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 40,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
