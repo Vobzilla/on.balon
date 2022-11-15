37756003eb6af413093a774c0a71c989fcfea342
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-11-15 21:08:13 +0100

    setting swiper

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 40,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  setWrapperSize: true,
 
   // Navigation arrows
   navigation: {
@@ -15,5 +16,17 @@ const swiper = new Swiper(".swiper", {
   keyboard: {
     enabled: true,
     onlyInViewport: false,
+  },
+  breakpoints: {
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    1050: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
