5f041ffe9de555f813ef4fb85cad5ec4a44e8719
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2024-09-05 22:16:58 +0200

    add img

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@
 // index.js
 import { initGallery } from "./gallery.js";
 import { initSlider } from "./slider.js";
+import { showScrollButton, scrollToTop } from "./scrollToTop.js";
 
 // Ініціалізація всіх модулів
 $(document).ready(function () {
   initGallery();
   initSlider();
 });
+
+showScrollButton();
+scrollToTop();
