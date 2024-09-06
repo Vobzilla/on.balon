408cb75e88adad0f1b02e7a3755a57b429ae3cc5
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2024-09-06 23:21:12 +0200

    add burger

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 import { initGallery } from "./gallery.js";
 import { initSlider } from "./slider.js";
 import { showScrollButton, scrollToTop } from "./scrollToTop.js";
+import { toggleBurgerMenu } from "./burger.js";
 
 // Ініціалізація всіх модулів
 $(document).ready(function () {
@@ -12,3 +13,4 @@ $(document).ready(function () {
 
 showScrollButton();
 scrollToTop();
+toggleBurgerMenu("burger-icon", "nav-mob");
